f9cf8093008798902a5e946c4557f1205696cf98
Renan K <2303779+rkuroki@users.noreply.github.com>
2023-03-29 11:51:24 -0300

    Update main.go

--- golang/simple-rest/main.go
+++ golang/simple-rest/main.go
@@ -1,93 +1,93 @@
 package main
 
 import (
-	"database/sql"
-	"fmt"
-	"log"
-	"net/http"
+  "database/sql"
+  "fmt"
+  "log"
+  "net/http"
 
-	"github.com/gin-gonic/gin"
-	_ "github.com/lib/pq"
+  "github.com/gin-gonic/gin"
+  _ "github.com/lib/pq"
 )
 
 const (
-	appHost = "localhost"
-	appPort = 8081
+  appHost = "localhost"
+  appPort = 8081
 
-	pgHost     = "localhost"
-	pgPort     = 53146
-	pgUser     = "quarkus"
-	pgPwd      = "quarkus"
-	pgDatabase = "quarkus"
+  pgHost     = "localhost"
+  pgPort     = 53146
+  pgUser     = "quarkus"
+  pgPwd      = "quarkus"
+  pgDatabase = "quarkus"
 )
 
 type Product struct {
-	ID           int     `json:"id"`
-	Name         string  `json:"name"`
-	Description  string  `json:"description"`
-	Price        float32 `json:"price"`
+  ID           int     `json:"id"`
+  Name         string  `json:"name"`
+  Description  string  `json:"description"`
+  Price        float32 `json:"price"`
 }
 
 func OpenDBConnection() *sql.DB {
-	psqlInfo := fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s sslmode=disable",
-		pgHost, pgPort, pgUser, pgPwd, pgDatabase)
+  psqlInfo := fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s sslmode=disable",
+      pgHost, pgPort, pgUser, pgPwd, pgDatabase)
 
-	db, err := sql.Open("postgres", psqlInfo)
-	if err != nil {
-		panic(err)
-	}
+  db, err := sql.Open("postgres", psqlInfo)
+  if err != nil {
+    panic(err)
+  }
 
-	err = db.Ping()
-	if err != nil {
-		panic(err)
-	}
+  err = db.Ping()
+  if err != nil {
+    panic(err)
+  }
 
-	return db
+  return db
 }
 
 func GETHandler(c *gin.Context) {
-	db := OpenDBConnection()
-	rows, err := db.Query("SELECT id, name, description, price FROM product")
-	if err != nil {
-		log.Fatal(err)
-	}
+  db := OpenDBConnection()
+  rows, err := db.Query("SELECT id, name, description, price FROM product")
+  if err != nil {
+    log.Fatal(err)
+  }
 
-	var products []Product
-	for rows.Next() {
-		var p Product
-		rows.Scan(&p.ID, &p.Name, &p.Description, &p.Price)
-		products = append(products, p)
-	}
-	c.IndentedJSON(http.StatusOK, products)
+  var products []Product
+  for rows.Next() {
+    var p Product
+    rows.Scan(&p.ID, &p.Name, &p.Description, &p.Price)
+    products = append(products, p)
+  }
+  c.IndentedJSON(http.StatusOK, products)
 
-	defer rows.Close()
-	defer db.Close()
+  defer rows.Close()
+  defer db.Close()
 }
 
 func POSTHandler(c *gin.Context) {
-	var p Product
-	if err := c.BindJSON(&p); err != nil {
-    	c.IndentedJSON(http.StatusBadRequest, "invalid product")
-        return
-    }
+  var p Product
+  if err := c.BindJSON(&p); err != nil {
+    c.IndentedJSON(http.StatusBadRequest, "invalid product")
+    return
+  }
 
-	db := OpenDBConnection()
-	sqlStatement := `INSERT INTO product (name, description, price) VALUES ($1, $2, $3)`
-	_, errDB := db.Exec(sqlStatement, p.Name, p.Description, p.Price)
-	if errDB != nil {
-    	c.IndentedJSON(http.StatusBadRequest, "error saving product")
-		panic(errDB)
-	}
-	c.IndentedJSON(http.StatusOK, p)
+  db := OpenDBConnection()
+  sqlStatement := `INSERT INTO product (name, description, price) VALUES ($1, $2, $3)`
+  _, errDB := db.Exec(sqlStatement, p.Name, p.Description, p.Price)
+  if errDB != nil {
+    c.IndentedJSON(http.StatusBadRequest, "error saving product")
+    panic(errDB)
+  }
+  c.IndentedJSON(http.StatusOK, p)
 
-	defer db.Close()
+  defer db.Close()
 }
 
 func main() {
-	router := gin.Default()
-    router.GET("/products", GETHandler)
-    router.POST("/products", POSTHandler)
+  router := gin.Default()
+  router.GET("/products", GETHandler)
+  router.POST("/products", POSTHandler)
 
-	routerHost := fmt.Sprintf("%s:%d", appHost, appPort)
-    router.Run(routerHost)
+  routerHost := fmt.Sprintf("%s:%d", appHost, appPort)
+  router.Run(routerHost)
 }
