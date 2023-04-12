d046349eb4d7c36dc1b4daea366c385150e89ee9
Takeshi Fukushima <takeshi.fukushima@cittati.com.br>
2023-04-11 23:15:26 -0300

    rest working with mocks

---
+++ kotlin/simple-api/src/main/kotlin/mtakeshi1/githut/io/model/DataAccess.kt
@@ -0,0 +1,11 @@
+package mtakeshi1.githut.io.model
+
+interface DataAccess {
+
+    fun listProducts(): List<Models.Product>
+    fun productWithId(id: Int): Models.Product
+
+    fun newOrder(items: List<Models.ShoppingCartEntry>): Models.ShoppingCart
+    fun orderWithId(id: Int): Models.ShoppingCart
+
+}
