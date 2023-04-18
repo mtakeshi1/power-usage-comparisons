aaa0f486d9c40becc816e3fc2f23abf20cf84b06
Takeshi Fukushima <takeshi.fukushima@cittati.com.br>
2023-04-17 22:06:39 -0300

    setting bind address to 0.0.0.0 instead of localhost

--- rust/axum_diesel/src/main.rs
+++ rust/axum_diesel/src/main.rs
@@ -37,7 +37,7 @@ async fn main() {
         .layer(middleware::map_response(log_error))
         .with_state(app_state);
 
-    let addr = SocketAddr::from(([127, 0, 0, 1], 8080));
+    let addr = SocketAddr::from(([0, 0, 0, 0], 8080));
     tracing::info!("Server started on {}", addr);
     axum::Server::bind(&addr)
         .serve(app.into_make_service())
